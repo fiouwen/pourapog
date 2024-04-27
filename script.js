79ec440c65cbb1daa3aae06fa36b0a3f53d59f91
fiouwen <168271325+fiouwen@users.noreply.github.com>
2024-04-27 12:57:13 +0200

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const yesButton = document.getElementById("yesButton");
 const noButton = document.getElementById("noButton");
-const imageContainer = document.getElementById("imageContainer");
+const chatImage = document.getElementById("chatImage");
 
 yesButton.addEventListener("click", () => {
-    imageContainer.style.opacity = "1";
+    weddingImage.classList.add("visible");
 });
 
 noButton.addEventListener("click", () => {
