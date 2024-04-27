5c3e4556d5f75719063793e4df72030398925c4f
fiouwen <168271325+fiouwen@users.noreply.github.com>
2024-04-27 13:50:06 +0200

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const fadeInButton = document.getElementById("fadeInButton");
 const noButton = document.getElementById("noButton");
 const imageContainer = document.getElementById("imageContainer");
-const weddingImage = document.getElementById("weddingImage");
 
 fadeInButton.addEventListener("click", () => {
             // Afficher l'image en augmentant l'opacité progressivement
-  imageContainer.style.opacity = "1";       
+alert("Félicitations pour notre mariage KYAAAA");
 });
 
 noButton.addEventListener("click", () => {
