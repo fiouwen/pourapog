b9c800c6c125cde3a1eb12503d39f424b85c14e4
fiouwen <168271325+fiouwen@users.noreply.github.com>
2024-04-27 12:49:49 +0200

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const noButton = document.getElementById("noButton");
 const imageContainer = document.getElementById("imageContainer");
 
 yesButton.addEventListener("click", () => {
-    alert("Je suis si heureuse my love");
     imageContainer.style.opacity = "1";
 });
 
